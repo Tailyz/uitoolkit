49865e3872c2c5f200b17ba63f963832a8ccdc8d
Karin Gagner <karin.gagner@edu.futuregames.nu>
2023-05-09 12:40:58 +0200

    Confirm quit function

--- UiToolKit/Assets/Ui/MainMenu/MainMenuUI.cs
+++ UiToolKit/Assets/Ui/MainMenu/MainMenuUI.cs
@@ -9,6 +9,7 @@ using UnityEngine.UIElements;
 [RequireComponent(typeof(UIDocument))]
 public class MainMenuUI : MonoBehaviour
 {
+    private VisualElement m_ConfirmationModal;
     private UIDocument m_UIDocument;
     [SerializeField] private LoadSceneChannel m_LoadSceneChannel;
     [SerializeField] private SceneReference m_StartingLocation;
@@ -32,8 +33,23 @@ public class MainMenuUI : MonoBehaviour
         Button exitButton = root.Q<Button>("exit-button"); 
         exitButton.clicked += QuitGame;
 
+        Button confirmButton = m_ConfirmationModal.Q<Button>("confirm-button");
+        confirmButton.clicked += QuitGame;
+
+        Button cancelButton = m_ConfirmationModal.Q<Button>("cancel-button");
+        cancelButton.clicked += Cancel;
+
+        Button closeButton = m_ConfirmationModal.Q<Button>("close-button");
+        closeButton.clicked += Cancel;
+
+
         Button newGameButton = root.Q<Button>("new-game-button"); 
         newGameButton.clicked += StartNewGame;
+    }
+
+        private void Cancel()
+    {
+        m_ConfirmationModal.style.display = DisplayStyle.None;
     }
 
     private void ContinuePreviousGame()
